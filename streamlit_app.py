c6d75b1e625bce666673d332cc52f460a0a7a5ae
Brenda Schutt <103045139+bschutt@users.noreply.github.com>
2023-11-23 21:39:02 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ with colm:
 st.markdown('---')
 
 # Replace this with your Looker dashboard URL
-looker_url = 'https://lookerstudio.google.com/reporting/2c58e29c-4ccc-43aa-a8ed-d55c4a12a9a1'
+looker_url = 'https://lookerstudio.google.com/reporting/2c58e29c-4ccc-43aa-a8ed-d55c4a12a9a1/page/p_zalhmmsqbd'
 
 def toLooker():
     webbrowser.open_new_tab(looker_url)
