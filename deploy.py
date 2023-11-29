2814f4d3c993deecab3fa17964a5b60fea9047cc
bschutt <103045139+bschutt@users.noreply.github.com>
2023-11-29 13:48:48 -0300

    deploy

--- deploy.py
+++ deploy.py
@@ -58,7 +58,7 @@ elif tema_seleccionado == "Preguntas":
           
         # Mapa de scripts por pregunta
 scripts = {
-   "¿Qué país tiene la esperanza de vida más alta para el 2040?": "scripts/pregunta_1.py",
+   "¿Qué país tiene la esperanza de vida más alta para el 2040?": "Deploy_streamlit/scripts/pregunta_1.py",
 
     # Agrega scripts para otras preguntas aquí
         }
